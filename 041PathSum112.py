8b267716170f39b1dfb00a13fb323254a62120a8
xlzhang <xlgzhang2012@gmail.com>
2015-06-22 16:37:50 -0400

    Create 041PathSum112.py

---
+++ 041PathSum112.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    # @param {TreeNode} root
+    # @param {integer} sum
+    # @return {boolean}
+    def hasPathSum(self, root, sum):
+        if not root:
+            return False
+        if root.val == sum and not root.left and not root.right:
+            return True
